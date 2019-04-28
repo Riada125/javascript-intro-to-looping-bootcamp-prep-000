b00d0c1c6782344c52abe520a3e8a000a660c52b
Michael Adair <riada125@hotmail.com>
2019-04-28 13:14:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,8 +20,15 @@ function whileLoop(n) {
 
 
 function doWhileLoop(num){
-  function incrementVariable()
+  function incrementVariable() {
+  i = i + 1;
+  return i;
 }
+}
+do {
+  console.log('I run once regardless');
+  } while (incrementVariable() < 10);
+  
 
 
 		
